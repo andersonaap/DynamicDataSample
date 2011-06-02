0700b8c3ab5adf3462195858b1507a7019ed5940
Anderson Pavia <andersonaap@hotmail.com>
2011-06-02 07:25:51 -0300

    Criacao

---
+++ VentoNorte/MetaDataClasses/VentoNorteMetaDados.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.DynamicData;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace VentoNorte
+{
+    [ScaffoldTable(false)]
+    public partial class Grupos { }
+    
+    [MetadataType(typeof(ProdutosMetaDados))]
+    public partial class Produtos { }
+
+    [TableName("Cadastro de Produtos")]
+    public class ProdutosMetaDados
+    {
+        [DisplayName("Nome")]
+        public object ProdName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:#.00}")]
+        [DisplayName("Preço")]
+        public object ProdPreco { get; set; }
+
+        [ScaffoldColumn(false)]
+        public object ProdEstoque { get; set; }
+    }
+
+    [MetadataType(typeof(FornecedoresMetaDados))]
+    public partial class Fornecedores { }
+
+    [TableName("Cadastro de Fornecedores")]
+    [DisplayColumn("FornNome")]
+    public class FornecedoresMetaDados {}
+
+}
---
+++ VentoNorte/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VentoNorte")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("VentoNorte")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("81aa93b8-4778-45b7-bf82-2d4e9f234b7d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
